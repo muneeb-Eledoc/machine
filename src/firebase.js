41c9e9b19283a0aa4aca1b5958d77f51b235ff47
muneeb-Eledoc <93725281+muneeb-Eledoc@users.noreply.github.com>
2023-05-02 09:47:33 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBD-iwiDWyjLFiM34lAV2B_Vl-DHuCL3nM",
-    authDomain: "machines-84e45.firebaseapp.com",
-    projectId: "machines-84e45",
-    storageBucket: "machines-84e45.appspot.com",
-    messagingSenderId: "272338097865",
-    appId: "1:272338097865:web:24a40f8b48277cca5b5f18",
-    measurementId: "G-9Y6CYR87CF"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
